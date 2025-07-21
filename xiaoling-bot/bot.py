49590b5f070f2b748352d67366b4e1117f7a0314
yuan-ie <kizxkie@gmail.com>
2025-07-21 17:30:07 -0400

    hello test success

--- xiaoling-bot/bot.py
+++ xiaoling-bot/bot.py
@@ -1,4 +1,8 @@
 import discord
+from discord.ext import commands
+
+# prefix to run a command for Xiaoling bot.
+bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
 
 keys = {}
 
@@ -11,32 +15,32 @@ with open('keys.txt', 'r') as file:
             else:
                 keys[key] = value
 
-print (keys['BOT_TOKEN'])
-print (keys['CHANNEL_ID'])
-
-# class MyClient(discord.Client):
-#     async def on_ready(self):
-#         print(f'Logged on as {self.user}!')
-
-#     async def on_message(self, message)
-#         print(f'Message from {message.author}: {message.content}')
+# print (keys['BOT_TOKEN'])
+# print (keys['CHANNEL_ID'])
 
 # intents = discord.Intents.default()
 # intents.message_content = True
 
 # client = MyClient(intents=intents)
 
-# # ------------- EVENTS ------------------
-# @client.event
-# async def on_ready():
-#     print(f'Logged on as {client.user}!')
+# ------------- EVENTS ------------------
+@bot.event
+async def on_ready():
+    print(f'Logged on as {bot.user}!')
+    channel = bot.get_channel(keys['CHANNEL_ID'])
+    await channel.send("Hello, I am at your service!")
 
+@bot.command()
+async def hello(ctx):
+    await ctx.send("Hello! :)")
 # @client.event
 # async def on_message(message):
 #     if message.author == client.user:
 #         return
 
-#     if message.content.startswith('$hello'):
-#         await message.channel.send('Hello!')
+    # if message.content.startswith('$hello'):
+    #     await message.channel.send('Hello!')
 
 # client.run(BOT_TOKEN)
+
+bot.run(keys['BOT_TOKEN'])
