482f77b64afce40263a06436aa52d2e28d58ca0c
yuan-ie <kizxkie@gmail.com>
2025-07-21 04:22:42 -0400

    hello dummy test

--- xiaoling-bot/bot.py
+++ xiaoling-bot/bot.py
@@ -1,2 +1,31 @@
-BOT_TOKEN = ""
-CHANNEL_ID = 1396759086267367444
+BOT_TOKEN = ''
+CHANNEL_ID =
+
+import discord
+
+# class MyClient(discord.Client):
+#     async def on_ready(self):
+#         print(f'Logged on as {self.user}!')
+
+#     async def on_message(self, message)
+#         print(f'Message from {message.author}: {message.content}')
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = MyClient(intents=intents)
+
+# ------------- EVENTS ------------------
+@client.event
+async def on_ready():
+    print(f'Logged on as {client.user}!')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+client.run(BOT_TOKEN)
