58da3e76781f46ec7b6766e69d41685dfb08ade5
HubHikariexe <mike.aleman@outlook.com>
2025-07-21 04:01:14 -0500

    test venv

--- xiaoling-bot/.venv/bin/activate.fish
+++ xiaoling-bot/.venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/kizxkie/kizxkie/yuans-discord-bot/xiaoling-bot/.venv
+set -gx VIRTUAL_ENV /home/maleman03/my-projects/yuans-discord-bot/xiaoling-bot/.venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
