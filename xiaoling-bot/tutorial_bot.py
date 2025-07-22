93bc6e274c3ce1aa87f81851db37a77f85ef2a91
yuan-ie <kizxkie@gmail.com>
2025-07-22 00:49:53 -0400

    embed stuff kinda works with the basics

--- xiaoling-bot/tutorial_bot.py
+++ xiaoling-bot/tutorial_bot.py
@@ -30,5 +30,58 @@ async def on_ready():
 async def hello(ctx):
     await ctx.send("Hello! :)")
 
+@bot.command()
+# async def embed(ctx):
+#     embed=discord.Embed(title="Embed Title", description="this is the embed title description", color=0xffabdc)
+#     # embed.set_thumbnail(url="https://i.imgur.com/axLm3p6.jpeg")
+#     embed.add_field(name="embed field 1", value="description")
+#     embed.add_field(name="embed field 2", value="description", inline=True)
+#     embed.add_field(name="embed field 3", value="description", inline=True)
+#     embed.set_footer(text="This is the footer. It contains text at the bottom of the embed")
+#     await ctx.send(embed=embed)
+
+async def embed(ctx, *args):
+    """
+    displays messages in embed. mainly for setting up rules and other important things.
+    usage: !embed <title> <description> <field1_name> <field1_value> <field2_name> <field2_value> ...
+    example: !embed `rules` `here are the rules` `rule #1`... 
+    """
+
+    if not args:
+        await ctx.send("Please enter the correct format: !embed `rules` `here are the rules` `rule #1`... ")
+        return
+
+    if len(args) >= 2:
+        title = args[0]
+        description = args[1]
+    else:
+        await ctx.send("At least put a title and description. :(")
+        return
+
+    embed=discord.Embed(
+        title=title,
+        description=description,
+        color=0xffabdc
+    )
+
+    for i in range(2, len(args), 2):
+        if i+1 < len(args):
+            field_name = args[i]
+            field_value = args[i+1]
+            embed.add_field(
+                name=field_name,
+                value=field_value,
+                inline=False
+                )
+
+    # embed=discord.Embed(title="Embed Title", description="this is the embed title description", color=0xffabdc)
+    # # embed.set_thumbnail(url="https://i.imgur.com/axLm3p6.jpeg")
+    # embed.add_field(name="embed field 1", value="description")
+    # embed.add_field(name="embed field 2", value="description", inline=True)
+    # embed.add_field(name="embed field 3", value="description", inline=True)
+    # embed.set_footer(text="This is the footer. It contains text at the bottom of the embed")
+    await ctx.send(embed=embed)
+    
+
 # RUN THE BOT
 bot.run(keys['BOT_TOKEN'])
