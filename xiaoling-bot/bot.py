0c137d943a65e944ac5fd42457731ebe19e633b9
yuan-ie <kizxkie@gmail.com>
2025-07-21 22:46:31 -0400

    added commments

--- xiaoling-bot/bot.py
+++ xiaoling-bot/bot.py
@@ -4,8 +4,10 @@ from discord.ext import commands
 # prefix to run a command for Xiaoling bot.
 bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
 
+# empty dictionary for the important stuff
 keys = {}
 
+# read the key textfile to get bot token and channel ID
 with open('keys.txt', 'r') as file:
     for line in file:
         if '=' in line:
@@ -15,32 +17,18 @@ with open('keys.txt', 'r') as file:
             else:
                 keys[key] = value
 
-# print (keys['BOT_TOKEN'])
-# print (keys['CHANNEL_ID'])
-
-# intents = discord.Intents.default()
-# intents.message_content = True
-
-# client = MyClient(intents=intents)
-
 # ------------- EVENTS ------------------
+# after starting up, bot says something in specified channel
 @bot.event
 async def on_ready():
     print(f'Logged on as {bot.user}!')
     channel = bot.get_channel(keys['CHANNEL_ID'])
     await channel.send("Hello, I am at your service!")
 
+# command to say hello
 @bot.command()
 async def hello(ctx):
     await ctx.send("Hello! :)")
-# @client.event
-# async def on_message(message):
-#     if message.author == client.user:
-#         return
 
-    # if message.content.startswith('$hello'):
-    #     await message.channel.send('Hello!')
-
-# client.run(BOT_TOKEN)
-
+# RUN THE BOT
 bot.run(keys['BOT_TOKEN'])
