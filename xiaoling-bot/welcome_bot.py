4d8727efcf4841910a9a3ecf2337ada22ecca923
HubHikariexe <mike.aleman@outlook.com>
2025-07-23 14:07:49 -0500

    welcome bot embed formatting

--- xiaoling-bot/welcome_bot.py
+++ xiaoling-bot/welcome_bot.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+from datetime import datetime
 
 bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
 keys = {}
@@ -27,18 +28,52 @@ async def on_member_join(member):
     #change to welcome channel ID
     channel = bot.get_channel(keys['CHANNEL_ID'])
     
+    #other channels
+    rules_channel_id = 1367685195154591864
+    intro_channel_id = 1367685212158296114
+    verify_channel_id = 1367688296653652079
     
-    #embedS
+    #setting up time
+    unix_time = int(datetime.utcnow().timestamp())
+    
+    
+    
+    #embeds
     embed = discord.Embed(
-        title ="Welcome to the yuan's starspace!",
-        description=f"Hello {member.mention}, glad to have you here!",
-        color=0x00ff00
+        title ="**welcome to『yuanie's server』 !**",
+        description=(
+            f"{member.mention} just joined! we hope you enjoy your time here :D\n\n"
+            "─✦─ ✧ ✦─\n\n"
+            f"make sure to read <#{rules_channel_id}>, make an <#{intro_channel_id}>, "
+            f"and <#{verify_channel_id}> before you chat!"
+        ),
+        color=discord.Color.from_rgb(74, 46, 100)
         
     )
     
-    embed.set_thumbnail(url="https://pbs.twimg.com/media/FsKbjMSaYAMAuuh.jpg")
+    
+    embed.set_author(
+        name=member.display_name,
+        icon_url=member.avatar.url if member.avatar else member.default_avatar.url
+    )
+    #embed.set_thumbnail(url="https://pbs.twimg.com/media/FsKbjMSaYAMAuuh.jpg")
+    embed.set_image(url="https://i.pinimg.com/originals/58/2a/8c/582a8c8f1941f193f32697a9d0dbca3c.gif")
     embed.add_field(name="Rules", value="Please check #rules!", inline=False)
-    embed.set_footer(text=f"Member #{len(member.guild.members)}")
+    embed.set_footer(text=f"we are now at {len(member.guild.members)} members! • <t:{unix_time}:f>")
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
     #sending embed to channel
     await channel.send(embed=embed)
