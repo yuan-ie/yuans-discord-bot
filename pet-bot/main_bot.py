353cc92fbdce1872e54d59d15b63027e0447a92f
yuan-ie <angela.nghiem1@gmail.com>
2025-08-09 01:42:34 -0400

    file function works well

--- pet-bot/main_bot.py
+++ pet-bot/main_bot.py
@@ -2,7 +2,18 @@ import discord
 from discord.ext import commands
 from datetime import datetime
 import random
+import csv
+import os
+from file_function import check_userinfo, add_userinfo, remove_userinfo, retrieve_info
 # import data.id_files as idfiles
+
+# create a file (if it doesn't exist that stores all the information)
+filename = "pet_datafile.csv"
+
+if not os.path.exists(filename):
+    with open(filename, mode="w", newline="") as file:
+        writer = csv.DictWriter(file, fieldnames=["userid", "pet_name", "date_adopted", "hearts_status"])
+        writer.writeheader()
 
 # prefix to run a command for Xiaoling bot.
 bot = commands.Bot(command_prefix="+", intents=discord.Intents.all())
@@ -25,31 +36,31 @@ with open('keys.txt', 'r') as file:
 # ------------------------------------------------------------
 
 class adoptModal(discord.ui.Modal):
-    def __init__(self, title:str, parent_view, message:discord.Message, petname="", author_id=""):
+    def __init__(self, title:str, parent_view, message:discord.Message, pet_name="", author_id=""):
         super().__init__(title=title)
 
         self.parent_view = parent_view # adoptMenu is the parent
         self.message = message
 
         # input pet name prompts
-        self.input_petname = discord.ui.TextInput(label="Pet Name", style=discord.TextStyle.short, default=petname, required=True)
-        self.add_item(self.input_petname)
+        self.input_pet_name = discord.ui.TextInput(label="Pet Name", style=discord.TextStyle.short, default=pet_name, required=True)
+        self.add_item(self.input_pet_name)
 
     # submit for each modal (not the submit button)
     async def on_submit(self, interaction: discord.Interaction):
 
         # pet name input from the user
-        usr_petname = self.input_petname.value
+        usr_pet_name = self.input_pet_name.value
 
         # saves the input to be used again
-        self.parent_view.prev_petname = usr_petname
+        self.parent_view.prev_pet_name = usr_pet_name
 
         # the existing embed
         embed = self.parent_view.embed
         embed.description = (
             "RIFU will allow you to have your very own virtual pet!\n"
             "Let's give your pet a name! ₊˚⊹ ᰔ\n\n"
-            f"You decided with **{usr_petname.upper()}**. Any last thoughts?\n"
+            f"You decided with **{usr_pet_name.upper()}**. Any last thoughts?\n"
             "*Note: You will not be able to change it later.*\n"
         )
         await self.message.edit(embed=self.parent_view.embed)
@@ -64,7 +75,7 @@ class adoptMenu(discord.ui.View):
         self.embed = embed
         self.channel = channel
         self.author_id = author_id
-        self.prev_petname = ""
+        self.prev_pet_name = ""
 
     # button to edit pet name
     @discord.ui.button(label="Pet Name", style=discord.ButtonStyle.grey)
@@ -73,7 +84,7 @@ class adoptMenu(discord.ui.View):
             title="Name your pet! 𐔌՞. .՞𐦯",
             parent_view=self,
             message=interaction.message,
-            petname=self.prev_petname,
+            pet_name=self.prev_pet_name,
             author_id=self.author_id
         )
         await interaction.response.send_modal(modal)
@@ -81,32 +92,27 @@ class adoptMenu(discord.ui.View):
     # button to confirm pet name
     @discord.ui.button(label="Confirm", style=discord.ButtonStyle.red)
     async def submit(self, interaction: discord.Interaction, button: discord.ui.Button):
-        if not self.prev_petname:
-            await interaction.response.send_message("You have not adopted a pet yet!", ephemeral=True)
-            return
-        
-        # store user and pet information
-        if self.author_id not in pets_data:
-            pets_data[self.author_id] = {}
-        pets_data[self.author_id]['pet_name'] = self.prev_petname.upper()
 
         # get the date and store the date in user information
         date_adopted = datetime.now().strftime("%B %d, %Y")
-        pets_data[self.author_id]['date_adopted'] = date_adopted
 
-        # pet heart status initialized to 0
-        pets_data[self.author_id]['hearts_status'] = 0
+        # initialize user's pet information (userid, pet name, date adopted, hearts status, and update flag(0 or 1))
+        value = add_userinfo(filename, self.author_id, self.prev_pet_name.upper(), date_adopted, 0, 0)
 
-        # success message
-        self.embed.description = f"Congratulations! You just adopted a new pet on **{date_adopted}** and decided to name it **{self.prev_petname.upper()}**!! ꉂ(˵˃ ᗜ ˂˵)!"
+        if value == 1:
+            # success message
+            self.embed.description = f"Congratulations! You just adopted a new pet on **{date_adopted}** and decided to name it **{self.prev_pet_name.upper()}**!! ꉂ(˵˃ ᗜ ˂˵)!"
 
-        # disable the buttons
-        for child in self.children:
-            if isinstance(child, discord.ui.Button):
-                child.disabled = True
-        await interaction.message.edit(embed=self.embed, view=self)
-        await interaction.response.send_message(f"You successfully adopted a new pet!", ephemeral=True)
-
+            # disable the buttons
+            for child in self.children:
+                if isinstance(child, discord.ui.Button):
+                    child.disabled = True
+            await interaction.message.edit(embed=self.embed, view=self)
+            await interaction.response.send_message(f"You successfully adopted a new pet!", ephemeral=True)
+        else:
+            await interaction.response.send_message("You have not adopted a pet yet!", ephemeral=True)
+            return
+        
 # ------------------------------------------------------------
 #                    USER FUNCTIONS
 # ------------------------------------------------------------
@@ -118,13 +124,13 @@ async def hello(ctx):
     await ctx.send("Hello! :)")
 
 @bot.command()
-async def petadopt(ctx):
+async def adopt(ctx):
     """
     Sends an embed to adopt a pet and name it.
     """
 
     author_id = ctx.author.id
-    pet = pets_data.get(author_id)
+    pet = check_userinfo(filename, author_id)
 
     # error check if there is no pet adopted
     if pet:
@@ -144,37 +150,39 @@ async def petadopt(ctx):
     await ctx.send(embed=embed,view=view)
 
 @bot.command()
-async def petinfo(ctx):
+async def info(ctx):
     """
     Sends an embed to view the info of the user's pet.
     """
     author_id = ctx.author.id
-    pet = pets_data.get(author_id)
+    pet = check_userinfo(filename, author_id)
 
     # error check if there is no pet adopted
     if not pet:
         await ctx.send("You have not adopted a pet yet!")
         return
     
+    user = retrieve_info(filename, author_id)
+    
     # display pet info
     embed = discord.Embed(
         title = "Pet Info \*ੈ✩‧₊˚",
         description = (
-            f"Name: **{pets_data[author_id]['pet_name']}** ₊˚⊹ ᰔ\n"
-            f"Date Adopted: {pets_data[author_id]['date_adopted']} \n"
+            f"Name: **{user['pet_name']}** ₊˚⊹ ᰔ\n"
+            f"Date Adopted: {user['date_adopted']} \n"
         ),
         color = 0x94c2ff
     )
     await ctx.send(embed=embed)
 
 @bot.command()
-async def petabandon(ctx):
+async def abandon(ctx):
     """
     Abandon pet...
     """
 
     author_id = ctx.author.id
-    pet = pets_data.get(author_id)
+    pet = check_userinfo(filename, author_id)
 
     # error check if there is no pet adopted
     if not pet:
@@ -182,39 +190,40 @@ async def petabandon(ctx):
         return
     
     # delete the user's pet information
-    del pets_data[author_id]
+    remove_userinfo(filename, author_id)
 
     await ctx.send("You have successfully abandoned the pet...")
 
 @bot.command()
-async def petstatus(ctx):
+async def status(ctx):
     """
     Display heart status of pet. It will increase with interactions.
     """
 
     author_id = ctx.author.id
-    pet = pets_data.get(author_id)
+    pet = check_userinfo(filename, author_id)
 
     # error check if there is no pet adopted
     if not pet:
         await ctx.send("You have not adopted a pet yet!")
         return
     
-    pet_name = pets_data[author_id]['pet_name']
+    user = retrieve_info(filename, author_id)
+    pet_name = user['pet_name']
 
-    match pets_data[author_id]['hearts_status']:
+    match float(user['hearts_status']):
         case x if x < 1:
             hearts_status = f"# ♡ ♡ ♡ ♡ ♡\n{pet_name} feels tense around you..."
         case x if x < 2:
-            hearts_status = f"# ❤︎ ♡ ♡ ♡ ♡\n{pet_name} is starting to warm up to you but still on edge."
+            hearts_status = f"# ♥︎ ♡ ♡ ♡ ♡\n{pet_name} is starting to warm up to you but still on edge."
         case x if x < 3:
-            hearts_status = f"# ❤︎ ❤︎ ♡ ♡ ♡\n{pet_name} is tolerating your existence."
+            hearts_status = f"# ♥︎ ♥︎ ♡ ♡ ♡\n{pet_name} is tolerating your existence."
         case x if x < 4:
-            hearts_status = f"# ❤︎ ❤︎ ❤︎ ♡ ♡\n{pet_name} feels safe with you and trusts you'll keep it alive."
+            hearts_status = f"# ♥︎ ♥︎ ♥︎ ♡ ♡\n{pet_name} feels safe with you and trusts you'll keep it alive."
         case x if x < 5:
-            hearts_status = f"# ❤︎ ❤︎ ❤︎ ❤︎ ♡\n{pet_name} likes you a lot and feels very comfortable with you."
+            hearts_status = f"# ♥︎ ♥︎ ♥︎ ♥︎ ♡\n{pet_name} likes you a lot and feels very comfortable with you."
         case x if x >= 5:
-            hearts_status = f"# ❤︎ ❤︎ ❤︎ ❤︎ ❤︎\n{pet_name} LOVES YOU SOOO MUCH!!"
+            hearts_status = f"# ♥︎ ♥︎ ♥︎ ♥︎ ♥︎\n{pet_name} LOVES YOU SOOO MUCH!!"
 
     embed = discord.Embed(
         title = "Pet Status\*ੈ✩‧₊˚",
@@ -226,30 +235,39 @@ async def petstatus(ctx):
     await ctx.send(embed=embed)
 
 @bot.command()
-async def petpet(ctx):
+async def pet(ctx):
     """
     Interact with pet by petting.
     """
 
     author_id = ctx.author.id
-    pet = pets_data.get(author_id)
+    pet = check_userinfo(filename, author_id)
 
     # error check if there is no pet adopted
     if not pet:
         await ctx.send("You have not adopted a pet yet!")
         return
     
-    pet_name = pets_data[author_id]['pet_name']
-    hearts_status = pets_data[author_id]['hearts_status']
+    user = retrieve_info(filename, author_id)
+    
+    pet_name = user['pet_name']
+    hearts_status = float(user['hearts_status'])
 
-    # pet_rng =  random.randint(1, 6 - int(hearts_status))
-    pet_rng = random.randint(0,1)
+    pet_rng =  random.randint(1, 6 - int(hearts_status))
     print(f'pet random number {pet_rng}!')
 
     if pet_rng == 1:
         
-        if hearts_status != 5:
-            pets_data[author_id]['hearts_status'] += 0.5
+        if hearts_status < 5:
+            hearts_status += 0.5
+        value = add_userinfo(
+            filename=filename,
+            userid=user['userid'],
+            pet_name=user['pet_name'],
+            date_adopted=user['date_adopted'],
+            hearts_status=hearts_status,
+            update=1
+        )
         await ctx.send(f"{pet_name} purrs and rolls around as you're petting.")
     else:
         await ctx.send(f"{pet_name} avoids your hand and glares at you...")
